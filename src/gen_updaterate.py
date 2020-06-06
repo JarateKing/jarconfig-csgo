557ded74aca5c93d13e618dd96dd9ac918b1f54f
JarateKing <scmacintosh@upei.ca>
2020-06-06 10:39:52 -0300

    added gen updaterate script

---
+++ src/gen_updaterate.py
@@ -0,0 +1,2 @@
+for i in range(1, 257):
+	print('alias jar_updaterate=', i, ' "cl_cmdrate ', i, '; cl_updaterate ', i, '"', sep='')
--- src/generate.bat
+++ src/generate.bat
@@ -1,3 +1,4 @@
-python gen_pitch.py > ../aliases/pitch.cfg
-python gen_yaw.py   > ../aliases/yaw.cfg
+python gen_pitch.py      > ../aliases/pitch.cfg
+python gen_yaw.py        > ../aliases/yaw.cfg
+python gen_updaterate.py > ../aliases/updaterate.cfg
 pause
