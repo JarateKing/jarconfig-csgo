881d1bb4ba02dfb2a3eaeed31547da473401abca
JarateKing <scmacintosh@upei.ca>
2020-06-06 14:17:18 -0300

    made sensitivity and yaw settings compatible

--- src/gen_sens.py
+++ src/gen_sens.py
@@ -3,4 +3,4 @@ def sens(dpi, inches, yaw):
 
 for inches in range(1, 41):
 	for dpi in range(400, 16001, 400):
-		print('alias jar_sens=', inches, 'in/360,', dpi, 'dpi "sensitivity ', sens(dpi, inches, 0.022), '"', sep='')
+		print('alias jar_sens=', inches, 'in/360,', dpi, 'dpi "sensitivity ', sens(dpi, inches, 0.022), '; alias jar_sens jar_sens=', inches, 'in/360,', dpi, 'dpi; jar_multsens"', sep='')
--- src/gen_yaw.py
+++ src/gen_yaw.py
@@ -1,3 +1,9 @@
+def invert(n):
+	if n == 0:
+		return 1
+	else:
+		return 1 / n
+
 for i in range(0, 101):
 	n = i / 10
-	print('alias jar_mouse_horiz_mult=', n, ' "m_yaw ', "{:.4f}".format(0.022 * n), '"', sep='')
+	print('alias jar_mouse_horiz_mult=', n, ' "m_yaw ', "{:.4f}".format(0.022 * n), '; alias jar_multsens multvar sensitivity 0 1000000 ', "{:.8f}".format(invert(n)), '; jar_sens; jar_multsens"', sep='')
