d67d884c80fc79fc53e3502f2ea5bb59d5fc44e6
JarateKing <scmacintosh@upei.ca>
2020-06-06 14:04:05 -0300

    added sens cfg

---
+++ src/gen_sens.py
@@ -0,0 +1,6 @@
+def sens(dpi, inches, yaw):
+	return 360 / (dpi * inches * yaw)
+
+for inches in range(1, 41):
+	for dpi in range(400, 16001, 400):
+		print('alias jar_sens=', inches, 'in/360,', dpi, 'dpi "sensitivity ', sens(dpi, inches, 0.022), '"', sep='')
--- src/generate.bat
+++ src/generate.bat
@@ -3,4 +3,5 @@ python gen_yaw.py        > ../aliases/yaw.cfg
 python gen_updaterate.py > ../aliases/updaterate.cfg
 python gen_interp.py     > ../aliases/interp.cfg
 python gen_zoom_sens.py  > ../aliases/zoom_sens.cfg
+python gen_sens.py       > ../aliases/sens.cfg
 pause
